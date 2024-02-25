38fe4fc6f763786aae1e669a83e107a8c5d20e5a
MauricioAlonso <program.alonso@gmail.com>
2024-02-25 13:00:15 -0300

    implementación lista de mongoose

--- app.js
+++ app.js
@@ -2,11 +2,10 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const errorController = require("./controllers/error");
-const mongoConnect = require("./util/database").mongoConnect
-const User = require('./models/user')
-
+const User = require("./models/user");
 
 const app = express();
 
@@ -20,9 +19,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-  User.findById("65d15e675c28c3ba6fd483c9")
-    .then(user => {
-      req.user = new User (user.name, user.email, user.cart, user._id);
+  User.findById("65daa2b16bf98bbf1fbf9aca")
+    .then((user) => {
+      req.user = user;
       next();
     })
     .catch((err) => console.log(err));
@@ -33,7 +32,27 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(()=>{
-
-  app.listen(3000)
-})
+//mauricioalonso
+//lQMIqjky1yWIUG8j
+mongoose
+  .connect(
+    "mongodb+srv://mauricioalonso:lQMIqjky1yWIUG8j@cluster0.lhmsbyf.mongodb.net/shop?retryWrites=true&w=majority"
+  )
+  .then((result) => {
+    User.findOne().then((user) => {
+      if (!user) {
+        const user = new User({
+          name: "Bauti",
+          email: "bauti@gmail.com",
+          cart: {
+            items: [],
+          },
+        });
+        user.save();
+      }
+    });
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
