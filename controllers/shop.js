1f3e98fb71698756760c2a72095448655c87229a
MauricioAlonso <program.alonso@gmail.com>
2024-03-14 00:26:03 -0300

    unidad lista

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,11 @@
+const fs = require("fs");
+const path = require("path");
+
+const PDFDocument = require("pdfkit");
+
 const Product = require("../models/product");
-const Order = require("../models/order")
+const Order = require("../models/order");
+const { errorMonitor } = require("stream");
 
 exports.getProducts = (req, res, next) => {
   Product.find()
@@ -85,7 +91,6 @@ exports.postCart = (req, res, next) => {
       error.httpStatusCode = 500;
       return next(error);
     });
-  
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
@@ -103,33 +108,33 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
-   req.user
-     .populate("cart.items.productId")
-     // .execPopulate()
-     .then((user) => {
-       const products = user.cart.items.map((i) => {
-         return { quantity: i.quantity, product: { ...i.productId._doc } };
-       });
-       const order = new Order({
-         user: {
-           email: req.user.email,
-           userId: req.user,
-         },
-         products: products,
-       });
-       return order.save();
-     })
-     .then((result) => {
-       return req.user.clearCart();
-     })
-     .then(() => {
-       res.redirect("/orders");
-     })
-     .catch((err) => {
-       const error = new Error(err);
-       error.httpStatusCode = 500;
-       return next(error);
-     });
+  req.user
+    .populate("cart.items.productId")
+    // .execPopulate()
+    .then((user) => {
+      const products = user.cart.items.map((i) => {
+        return { quantity: i.quantity, product: { ...i.productId._doc } };
+      });
+      const order = new Order({
+        user: {
+          email: req.user.email,
+          userId: req.user,
+        },
+        products: products,
+      });
+      return order.save();
+    })
+    .then((result) => {
+      return req.user.clearCart();
+    })
+    .then(() => {
+      res.redirect("/orders");
+    })
+    .catch((err) => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
 };
 
 exports.getOrders = (req, res, next) => {
@@ -146,4 +151,67 @@ exports.getOrders = (req, res, next) => {
       error.httpStatusCode = 500;
       return next(error);
     });
+};
+
+exports.getInvoice = (req, res, next) => {
+  const orderId = req.params.orderId;
+  Order.findById(orderId)
+    .then((order) => {
+      if (!order) {
+        return next(new Error("No order found."));
+      }
+      if (order.user.userId.toString() !== req.user._id.toString()) {
+        return next(new Error("Unauthorized."));
+      }
+      const invoiceName = "invoice-" + orderId + ".pdf";
+      const invoicePath = path.join("data", "invoices", invoiceName);
+
+      const pdfDoc = new PDFDocument();
+      res.setHeader("Content-Type", "application/pdf");
+      res.setHeader(
+        "Content-Disposition",
+        'inline; filename ="' + invoiceName + '"'
+      );
+      pdfDoc.pipe(fs.createWriteStream(invoicePath));
+      pdfDoc.pipe(res);
+
+      pdfDoc.fontSize(26).text("Invoice", {
+        underline: true,
+      });
+
+      pdfDoc.text("--------------------------------");
+      let totalPrice = 0;
+      order.products.forEach((prod) => {
+        totalPrice += prod.quantity * prod.product.price;
+        pdfDoc
+          .fontSize(14)
+          .text(
+            prod.product.title +
+              " - " +
+              prod.quantity +
+              " x " +
+              "$" +
+              prod.product.price
+          );
+      });
+      pdfDoc.text("-----");
+      pdfDoc.fontSize(20).text("Total Price: $" + totalPrice);
+
+      pdfDoc.end();
+      // fs.readFile(invoicePath, (err, data) => {
+      //   if (err) {
+      //     return next(err);
+      //   }
+      //   res.setHeader("Content-Type", "application/pdf");
+      //   res.setHeader(
+      //     "Content-Disposition",
+      //     'inline; filename ="' + invoiceName + '"'
+      //   );
+      //   res.send(data);
+      // });
+      // const file = fs.createReadStream(invoicePath);
+
+      // file.pipe(res)
+    })
+    .catch((err) => next(err));
 };
