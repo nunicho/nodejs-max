0c38cee7d0ccca9bac29e17e24bf409e30d3a122
MauricioAlonso <program.alonso@gmail.com>
2024-03-15 00:21:04 -0300

    listo

--- controllers/shop.js
+++ controllers/shop.js
@@ -5,23 +5,39 @@ const PDFDocument = require("pdfkit");
 
 const Product = require("../models/product");
 const Order = require("../models/order");
-const { errorMonitor } = require("stream");
+
+const ITEMS_PER_PAGE = 2;
 
 exports.getProducts = (req, res, next) => {
-  Product.find()
-    .then((products) => {
-      console.log(products);
-      res.render("shop/product-list", {
-        prods: products,
-        pageTitle: "All Products",
-        path: "/products",
-      });
-    })
-    .catch((err) => {
-      const error = new Error(err);
-      error.httpStatusCode = 500;
-      return next(error);
-    });
+ const page = +req.query.page || 1; // se pone +req para que sea de formato numérico
+ let totalItems;
+
+ Product.find()
+   .countDocuments()
+   .then((numProducts) => {
+     totalItems = numProducts;
+     return Product.find()
+       .skip((page - 1) * ITEMS_PER_PAGE)
+       .limit(ITEMS_PER_PAGE);
+   })
+   .then((products) => {
+     res.render("shop/product-list", {
+       prods: products,
+       pageTitle: "Products",
+       path: "/products",
+       currentPage: page,
+       hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+       hasPreviousPage: page > 1,
+       nextPage: page + 1,
+       previousPage: page - 1,
+       lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE),
+     });
+   })
+   .catch((err) => {
+     const error = new Error(err);
+     error.httpStatusCode = 500;
+     return next(error);
+   });
 };
 
 exports.getProduct = (req, res, next) => {
@@ -42,12 +58,28 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
+  const page = +req.query.page || 1;  // se pone +req para que sea de formato numérico
+  let totalItems; 
+
   Product.find()
+    .countDocuments()
+    .then((numProducts) => {
+      totalItems = numProducts
+      return Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
+    })
     .then((products) => {
       res.render("shop/index", {
         prods: products,
         pageTitle: "Shop",
         path: "/",
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * page <totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage : page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
       });
     })
     .catch((err) => {
